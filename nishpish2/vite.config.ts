5a63acb5b1aacdcc222ef46010ad8cec0f0eb395
Kshitijv112 <kshitijv112@gmail.com>
2025-08-12 11:58:09 +0530

    vite.config.ts

--- nishpish2/vite.config.ts
+++ nishpish2/vite.config.ts
@@ -4,7 +4,7 @@ import react from '@vitejs/plugin-react';
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: "/Studysync1/",
+  base: "/Studysync1/nishpish2",
   optimizeDeps: {
     exclude: ['lucide-react'],
   },
